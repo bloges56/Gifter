1f5ce8d98d4965724b85d81a5c586ba8dfd53c88
bloges56 <brady.drew.logan@gmail.com>
2021-01-12 10:53:06 -0600

    deleting a user now deletes all of their posts and comments

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -18,6 +18,8 @@ namespace Gifter.Models
 
         [Required]
         public DateTime DateCreated { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Post> Posts { get; set; }
 
     }
 }
